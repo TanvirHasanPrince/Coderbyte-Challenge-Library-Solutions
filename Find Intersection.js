95e6cd7e524574d063f34632538df2d968d57a26
unknown <t.hasan.prince@gmail.com>
2024-02-04 12:46:43 +0600

    Find Intersection-Method 2

--- Find Intersection.js
+++ Find Intersection.js
@@ -19,6 +19,20 @@ function FindIntersection(strArr) {
 // keep this function call here
 console.log(FindIntersection(readline()));
 
+
+//Another Method:
+function FindIntersection(strArr) {
+  const firstElement = strArr[0].split(", ");
+  const secondElement = strArr[1].split(", ");
+  var result = firstElement.filter((number1) =>
+    secondElement.find((number2) => number2 === number1)
+  );
+  return result.length > 0 ? result.join(",") : "false";
+}
+
+// keep this function call here
+console.log(FindIntersection(readline()));
+
 /*
 Find Intersection
 Have the function FindIntersection(strArr) read the array of strings stored in strArr which will contain 2 elements: the first element will represent a list of comma-separated numbers sorted in ascending order, the second element will represent a second list of comma-separated numbers (also sorted). Your goal is to return a comma-separated string containing the numbers that occur in elements of strArr in sorted order. If there is no intersection, return the string false.
@@ -31,3 +45,5 @@ Input: ["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"]
 Output: 1,9,10
 
 */
+
+
