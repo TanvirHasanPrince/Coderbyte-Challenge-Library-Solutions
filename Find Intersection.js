0abdf292492742f9ecea95747b1ca7c53ce177e3
unknown <t.hasan.prince@gmail.com>
2024-02-04 11:28:36 +0600

    Find Intersection

--- Find Intersection.js
+++ Find Intersection.js
@@ -18,3 +18,9 @@ function FindIntersection(strArr) {
 
 // keep this function call here
 console.log(FindIntersection(readline()));
+
+/*
+
+Find Intersection
+Have the function FindIntersection(strArr) read the array of strings stored in strArr which will contain 2 elements: the first element will represent a list of comma-separated numbers sorted in ascending order, the second element will represent a second list of comma-separated numbers (also sorted). Your goal is to return a comma-separated string containing the numbers that occur in elements of strArr in sorted order. If there is no intersection, return the string false.
+*/
