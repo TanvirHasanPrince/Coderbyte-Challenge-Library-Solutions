2637c7a87bda27cbb9cc85b42047c447152adf94
unknown <t.hasan.prince@gmail.com>
2024-02-07 01:08:22 +0600

    First Reverse- Recursion Explained

--- First Reverse.js
+++ First Reverse.js
@@ -8,7 +8,6 @@ Output: edoC evoL I
 
 */
 
-
 //First Method => Split, Reverse and join
 function FirstReverse(str) {
   const deconstructed = str.split("");
@@ -46,3 +45,47 @@ function FirstReverse(str) {
 
 // keep this function call here
 console.log(FirstReverse(readline()));
+
+/*
+Input => Coderbyte
+This is not an empty string. So it will go to else block. 
+So the FirstReverse is getting “oderbyte” & str.chartAt(0) is “c” here
+FirstReverse is getting “derbyte” & str.chartAt(0) is “o” here
+FirstReverse is getting “erbyte” & str.chartAt(0) is “d” here
+FirstReverse is getting “rbyte” & str.chartAt(0) is “e” here
+FirstReverse is getting “byte” & str.chartAt(0) is “r” here
+FirstReverse is getting “yte” & str.chartAt(0) is “b” here
+FirstReverse is getting “te” & str.chartAt(0) is “y” here
+FirstReverse is getting “e” & str.chartAt(0) is “t” here
+FirstReverse is getting “” & will return “” and str.chartAt(0) is “e” here
+
+
+Initial Call: FirstReverse("") + "e"
+FirstReverse("") returns "" (base case).
+"" + "e" results in "e".
+Recursive Call: FirstReverse("e") + "t"
+FirstReverse("e") returns "e" (base case).
+"e" + "t" results in "et".
+Recursive Call: FirstReverse("te") + "y"
+FirstReverse("te") returns "et" (reversed).
+"et" + "y" results in "ety".
+Recursive Call: FirstReverse("yte") + "b"
+FirstReverse("yte") returns "ety" (reversed).
+"ety" + "b" results in "etyb".
+Recursive Call: FirstReverse("byte") + "r"
+FirstReverse("byte") returns "etyb" (reversed).
+"etyb" + "r" results in "etybr".
+Recursive Call: FirstReverse("rbyte") + "o"
+FirstReverse("rbyte") returns "etybr" (reversed).
+"etybr" + "o" results in "etybro".
+Recursive Call: FirstReverse("erbyte") + "d"
+FirstReverse("erbyte") returns "etybro" (reversed).
+"etybro" + "d" results in "etybrod".
+Recursive Call: FirstReverse("derbyte") + "o"
+FirstReverse("derbyte") returns "etybrod" (reversed).
+"etybrod" + "o" results in "etybrodo".
+Recursive Call: FirstReverse("oderbyte") + "C"
+FirstReverse("oderbyte") returns "etybrodo" (reversed).
+"etybrodo" + "C" results in "etybrodoC"
+
+*/
