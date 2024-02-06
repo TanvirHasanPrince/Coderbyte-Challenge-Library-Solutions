d5944287c52e159245437386075edfd7f3994315
unknown <t.hasan.prince@gmail.com>
2024-02-07 01:52:50 +0600

    Longest Word

---
+++ Longest Word.js
@@ -0,0 +1,26 @@
+/*
+Longest Word
+Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"
+Examples
+Input: "fun&!! time"
+Output: time
+Input: "I love dogs"
+Output: love
+
+*/
+
+function LongestWord(sen) {
+  let withoutPunctuation = sen.replace(/[^\w\s]|_/g, "");
+  const wordlist = withoutPunctuation.split(" ");
+  let longestWord = "";
+
+  for (let i = 0; i < wordlist.length; i++) {
+    if (wordlist[i].length > longestWord.length) {
+      longestWord = wordlist[i];
+    }
+  }
+  return longestWord;
+}
+
+// keep this function call here
+console.log(LongestWord(readline()));
