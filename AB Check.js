b829f9682bff983d3cf5d577ddb08eea9d514d29
unknown <t.hasan.prince@gmail.com>
2024-02-15 21:56:11 +0600

    AB Check-Another Method

--- AB Check.js
+++ AB Check.js
@@ -15,3 +15,32 @@ function ABCheck(str) {
 
 // keep this function call here
 console.log(ABCheck(readline()));
+
+//Another method-checking for Edge case
+function ABCheck(str) {
+  // convert the whole string to lowercase letters
+  var str = str.toLowerCase();
+
+  // loop through the string
+  for (var i = 0; i < str.length; i++) {
+    // check for "a...b"
+    if (str[i] === "a" && str[i + 4] === "b") {
+      return true;
+    }
+
+    // check for "b...a"
+    if (str[i] === "b" && str[i + 4] === "a") {
+      return true;
+    }
+
+    // NOTE: in JavaScript we don't have to worry about out-of-bounds errors
+    // because it just returns undefined and the program keeps running
+  }
+
+  // if none of above were encountered
+  // then return false by default
+  return false;
+}
+
+// keep this function call here
+ABCheck("Laura sobs");
